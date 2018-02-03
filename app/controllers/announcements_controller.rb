9c8f08dbe8799890f7f0ed7ecc6d65b0f957c3ed
Guek Ling <yeoyueling@gmail.com>
2018-02-03 14:58:35 +0800

    Fix Rubocop offenses

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,7 +1,8 @@
-class AnnouncementsController < ApplicationController
-	def new 
-		@announcements = Announcement.new
-		@users = User.all
-	end
+# frozen_string_literal: true
 
+class AnnouncementsController < ApplicationController
+  def new
+    @announcements = Announcement.new
+    @users = User.all
+  end
 end
