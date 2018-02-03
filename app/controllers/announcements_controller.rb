286b25cfd4f72f044986a93ddc947f527188391a
Guek Ling <yeoyueling@gmail.com>
2018-02-03 15:09:25 +0800

    Add 'index' method

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class AnnouncementsController < ApplicationController
+  def index
+  	@announcements = Announcement.all
+  end
+
   def new
     @announcements = Announcement.new
     @users = User.all
