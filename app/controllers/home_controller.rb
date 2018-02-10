54e9e01379d76103ae010ace99e367fcead978ba
Jiang Yixing <jiangyixing9@gmail.com>
2018-02-09 20:27:55 -0700

    update home#index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
 	before_action :authenticate_user!
 	def index
+
 		if user_signed_in?
+			@uu=current_user
 			#redirect_to new_user_session_path
 		else
 			redirect_to new_user_session_path
