9127355e2fff93cb9e79c26ec04ed981451f9a15
Sergio Vieri <sergio@vieri.tech>
2018-02-10 15:02:17 +0800

    Added table and update availability

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -9,10 +9,9 @@ class AvailabilitiesController < ApplicationController
     @availabilities = []
     time_ranges = TimeRange.all
     for day in 0..6 do
-      puts day
       cur = []
       for time_range in time_ranges do
-        tmp = Availability.where(user_id: 1, time_range_id: time_range.id, day: day)[0]
+        tmp = Availability.where(user_id: current_user.id, time_range_id: time_range.id, day: day)[0]
         if tmp.nil?
           tmp = create_new_availability(time_range.id, day)
         end
@@ -24,15 +23,45 @@ class AvailabilitiesController < ApplicationController
 
   def create_new_availability(time_range_id, day)
     tmp = Availability.new
-    tmp.user_id = 1
+    tmp.user_id = current_user.id
     tmp.day = day
     tmp.time_range_id = time_range_id
     tmp.status = 0
-    if tmp.save
-    else
-      puts 'fail'
-    end
+    tmp.save
     tmp
   end
+
+  def create
+
+    @availabilities = []
+    time_ranges = TimeRange.all
+    for day in 0..6 do
+      cur = []
+      for time_range in time_ranges do
+        tmp = Availability.where(user_id: current_user.id, time_range_id: time_range.id, day: day)[0]
+        if tmp.nil?
+          tmp = create_new_availability(time_range.id, day)
+        end
+        cur.push(tmp)
+      end
+      @availabilities.push(cur)
+    end
+
+    for i in @availabilities do
+      for j in i do
+        if params[:availability_ids].include? j.id.to_s
+          set(j, 1)
+        else
+          set(j, 0)
+        end
+      end
+    end
+  end
+
+  def set(x, y)
+    x.status = y
+    x.save
+  end
+
 
 end
