05fc3878fb485ab2a7962e3aaaea4ded1f14fc2f
Sergio Vieri <sergio@vieri.tech>
2018-02-11 09:15:59 +0800

    Rubocop passed

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,16 +1,17 @@
+# frozen_string_literal: true
+
 class AvailabilitiesController < ApplicationController
   before_action :authenticate_user!
   def index
-    @availabilities = load_availabilities()
+    @availabilities = load_availabilities
   end
 
   def create
     Availability.where(user_id: current_user.id).each do |availability|
-      set(availability, params[:availability_ids].include?(availability.id.to_s))
+      set(availability,
+          params[:availability_ids].include?(availability.id.to_s))
     end
   end
-
-private
 
   def set(availability, status)
     availability.status = status ? 1 : 0
@@ -32,15 +33,13 @@ private
   end
 
   def get_availability(day, time_range_id)
-    availability = Availability.where(user_id: current_user.id, day: day,
-        time_range_id: time_range_id)[0]
-    if availability.nil?
-      availability = create_new_availability(day, time_range_id)
-    availability
+    Availability.where(user_id: current_user.id, day: day,
+                       time_range_id: time_range_id)[0] ||
+      create_new_availability(day, time_range_id)
   end
 
   def create_new_availability(day, time_range_id)
     Availability.create(user_id: current_user.id, day: day,
-        time_range_id: time_range_id)
+                        time_range_id: time_range_id)
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,12 @@
-class HomeController < ApplicationController
-	before_action :authenticate_user!
-	def index
-		if user_signed_in?
-			#redirect_to new_user_session_path
-		else
-			redirect_to new_user_session_path
-		end
+# frozen_string_literal: true
 
-	end
+class HomeController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    if user_signed_in?
+      # redirect_to new_user_session_path
+    else
+      redirect_to new_user_session_path
+    end
+  end
 end
