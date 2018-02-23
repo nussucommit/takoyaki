284271b99f665cfcaf185ccd1e4d27a0a1794042
Guek Ling <yeoyueling@gmail.com>
2018-02-23 10:42:52 +0800

    Resolve conflict in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,15 @@ ActiveRecord::Schema.define(version: 20180210035022) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "announcements", force: :cascade do |t|
+    t.datetime "date"
+    t.bigint   "user_id",    :index=>{:name=>"index_announcements_on_user_id"}
+    t.text     "subject"
+    t.text     "details"
+    t.datetime "created_at", :null=>false
+    t.datetime "updated_at", :null=>false
+  end
+
   create_table "duties", force: :cascade do |t|
     t.bigint   "user_id",     :index=>{:name=>"index_duties_on_user_id"}
     t.bigint   "timeslot_id", :index=>{:name=>"index_duties_on_timeslot_id"}
@@ -61,6 +70,7 @@ ActiveRecord::Schema.define(version: 20180210035022) do
     t.datetime "updated_at",             :null=>false
   end
 
+  add_foreign_key "announcements", "users"
   add_foreign_key "duties", "timeslots"
   add_foreign_key "duties", "users"
   add_foreign_key "timeslots", "places"
---
+++ spec/factories/announcements.rb
@@ -0,0 +1,31 @@
+# rubocop:disable Metrics/LineLength
+# == Schema Information
+#
+# Table name: announcements
+#
+#  id         :integer          not null, primary key
+#  date       :datetime
+#  user_id    :integer
+#  subject    :text
+#  details    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_announcements_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+# rubocop:enable Metrics/LineLength
+
+FactoryBot.define do
+  factory :announcement do
+    date "2018-02-03 14:37:02"
+    user nil
+    subject "MyText"
+    details "MyText"
+  end
+end
