e6d1b8788e9d5c548406168a4fdd12f67078be33
Guek Ling <yeoyueling@gmail.com>
2018-02-23 10:35:24 +0800

    Delete comments and change timezone in controller

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -5,27 +5,23 @@ class AnnouncementsController < ApplicationController
     @announcements = Announcement.all
   end
 
-  # show
   def show
     @announcement = Announcement.find params[:id]
   end
 
-  # new
   def create
-    announcement = Announcement.new announcement_new_params
-    announcement.date = DateTime.now.getlocal
+    announcement = Announcement.new announcement_params
+    announcement.date = Time.zone.now
 
     redirect_to announcements_path if announcement.save
   end
 
-  # edit
   def update
     announcement = Announcement.find params[:id]
 
-    redirect_to announcements_path if announcement.update announcement_edit_params
+    redirect_to announcements_path if announcement.update announcement_params
   end
 
-  # delete
   def destroy
     announcement = Announcement.find params[:id]
 
@@ -34,11 +30,7 @@ class AnnouncementsController < ApplicationController
 
   private
 
-  def announcement_new_params
-    params.require(:announcement).permit(:subject, :details)
-  end
-
-  def announcement_edit_params
+  def announcement_params
     params.require(:announcement).permit(:subject, :details)
   end
 end
