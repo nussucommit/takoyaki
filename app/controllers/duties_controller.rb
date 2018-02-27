d4db2c6655f2c7f4c5ce37fb33e20cfc61d43dc5
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-02-27 18:16:01 +0800

    Implement frontend and Add fake data

--- app/controllers/duties_controller.rb
+++ app/controllers/duties_controller.rb
@@ -3,7 +3,9 @@
 class DutiesController < ApplicationController
   def index
     time_range = TimeRange.order(:start_time)
-    @first = time_range.first.start_time
-    @last = time_range.last.end_time
+    first = time_range.first.start_time
+    @header_iter = first.to_i.step(time_range.last.start_time.to_i, 3600)
+    # upper-bound exclusive, hence subtract by 1
+    @timetable_iter = first.to_i.step(time_range.last.end_time.to_i - 1, 1800)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # Helper to choose foreground colour from a given background colour
+  # following W3C Recommendation https://www.w3.org/TR/WCAG20/#relativeluminancedef
+  def choose_fgcolour(bgcolour)
+    r, g, b = srgb_to_linearrgb(bgcolour)
+    luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b
+    luminance > 0.179 ? '#000000' : '#FFFFFF'
+  end
+
+  private
+
+  def srgb_to_linearrgb(colour)
+    r, g, b = colour.match(/#(..)(..)(..)/).to_a.drop(1).map do |e|
+      a = e.hex / 255
+      a <= 0.03928 ? a / 12.92 : ((a + 0.055) / 1.055)**2.4
+    end
+    [r, g, b]
+  end
 end
