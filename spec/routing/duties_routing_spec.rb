39b4ceface2ff5c5bfb2d87ba526a147ec7b775a
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-03-03 22:25:11 +0800

    Add duties routing test

---
+++ spec/routing/duties_routing_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe DutiesController, type: :routing do
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(get: '/duties').to route_to('duties#index')
+    end
+    it 'routes to #generate_duties' do
+      expect(post: '/duties/generate').to route_to('duties#generate_duties')
+    end
+  end
+end
