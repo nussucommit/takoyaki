45fde53ed491c361195184f0c7b2be448eb54edf
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-03-03 22:18:49 +0800

    Change generate route

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  post '/generate', to: 'duties#generate_duties'
-  resources :duties
+  resources :duties do
+    collection do
+      post 'generate', to: 'duties#generate_duties'
+    end
+  end
 
   resources :announcements, only: %i[index create destroy update]
 end
