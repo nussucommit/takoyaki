ce5389a0ddc238a7f68b9ed6ab611928ece33b54
Herbert Ilhan Tanujaya <mail@herbert.id>
2018-03-03 14:21:21 +0800

    Separate adding FactoryBot method

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,7 @@ if Rails.env.production?
 end
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'support/factory_bot'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -58,9 +59,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  # Include FactoryBot
-  config.include FactoryBot::Syntax::Methods
 end
 
 Shoulda::Matchers.configure do |config|
