d567ea6dad8427822f9a7d731ac471002c0c5124
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-03-03 15:28:05 +0800

    Done duty view

--- app/controllers/duties_controller.rb
+++ app/controllers/duties_controller.rb
@@ -7,14 +7,13 @@ class DutiesController < ApplicationController
     @header_iter = first.to_i.step(time_range.last.start_time.to_i, 3600)
     # upper-bound exclusive, hence subtract by 1
     @timetable_iter = first.to_i.step(time_range.last.end_time.to_i - 1, 1800)
-
-    start_date = Date.today.beginning_of_week
-    end_date = start_date + 6.day
-    places = Place.all
-    @date_array = (start_date)
-    @duty_array = (start_date..end_date).to_a.map do |date|
-      places.map do |place|
-        Duty.where(date: date).joins(:timeslot).where('timeslots.place' => place)
+    @start_date = Time.zone.today.beginning_of_week
+    @end_date = @start_date + 6.days
+    @places = Place.all
+    @duty_array = (@start_date..@end_date).to_a.map do |date|
+      @places.map do |place|
+        Duty.where(date: date)
+            .joins(:timeslot).where('timeslots.place' => place)
       end
     end
   end
