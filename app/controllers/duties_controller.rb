a399cf2e010db95d3f73e60d0cf8762e0e0a8a8f
Sreyans Sipani <sreyans.sipani99@gmail.com>
2018-03-03 15:32:08 +0800

    Fix conflict

--- app/controllers/duties_controller.rb
+++ app/controllers/duties_controller.rb
@@ -4,17 +4,18 @@ class DutiesController < ApplicationController
   def index
     time_range = TimeRange.order(:start_time)
     first = time_range.first.start_time
-    @header_iter = first.to_i.step(time_range.last.start_time.to_i, 3600)
+    @header_iter = first.to_i.step(time_range.last.start_time.to_i, 1.hour)
     # upper-bound exclusive, hence subtract by 1
     @timetable_iter = first.to_i.step(time_range.last.end_time.to_i - 1, 1800)
     @start_date = Time.zone.today.beginning_of_week
     @end_date = @start_date + 6.days
     @places = Place.all
     @duty_array = (@start_date..@end_date).to_a.map do |date|
-      @places.map do |place|
+      @places.to_a.map do |place|
         Duty.where(date: date)
-            .joins(:timeslot).where('timeslots.place' => place)
+            .joins(:timeslot).where('timeslots.place' => place).to_a
       end
     end
   end
 end
+  
