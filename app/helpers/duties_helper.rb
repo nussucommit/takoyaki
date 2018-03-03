0c6ea0e71bdb960b20490d3f66f0023735097ba4
Sreyans Sipani <sreyans.sipani99@gmail.com>
2018-03-03 15:29:52 +0800

    Added helper method to add dummy duties

--- app/helpers/duties_helper.rb
+++ app/helpers/duties_helper.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
 module DutiesHelper
+
+	def addDummyDuties(duty_array) 
+
+		TimeRange.all.each do |tr| 
+			Place.all.each do |place|
+				unless Timeslot.find_by({time_range: tr, place: place})	
+					duty_array.each_with_index do |duty_array_for_each_day, index|
+						duty_array[index][place.id - 1].insert(tr.id - 1, "dummy")
+					end
+				end
+			end
+		end
+	end
+
 end
