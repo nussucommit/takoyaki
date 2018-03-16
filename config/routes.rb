be5aee299ad1619235d443707795f671e88522dd
Herbert Ilhan Tanujaya <mail@herbert.id>
2018-03-17 00:43:10 +0800

    Cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'home#index'
+  root to: 'duties#index'
 
   devise_for :users, controllers: { registrations: 'registrations' }
 
--- test/factories/admins.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :admin do
-  end
-end
