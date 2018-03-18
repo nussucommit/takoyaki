47d8e22068b31609ddc253e792893cfc0f84070e
Herbert Ilhan Tanujaya <mail@herbert.id>
2018-03-19 00:13:38 +0800

    Remove extra "describe" in rspec

--- spec/routing/availabilities_routing_spec.rb
+++ spec/routing/availabilities_routing_spec.rb
@@ -3,15 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe AvailabilitiesController, type: :routing do
-  describe 'routing' do
-    it 'routes to #index' do
-      expect(get: '/availabilities').to route_to('availabilities#index')
-    end
+  it 'routes to #index' do
+    expect(get: '/availabilities').to route_to('availabilities#index')
+  end
 
-    it 'routes to #update_availabilities' do
-      expect(post: '/availabilities').to route_to(
-        'availabilities#update_availabilities'
-      )
-    end
+  it 'routes to #update_availabilities' do
+    expect(post: '/availabilities').to route_to(
+      'availabilities#update_availabilities'
+    )
   end
 end
