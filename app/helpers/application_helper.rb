09660e8c124968114f774505dbda7a16bf38fc11
Herbert Ilhan Tanujaya <mail@herbert.id>
2018-03-19 00:21:17 +0800

    Refactor helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def calc_colspan(start_time, end_time)
+    ((end_time - start_time) / 0.5.hours).round
+  end
 end
--- app/helpers/availabilities_helper.rb
+++ app/helpers/availabilities_helper.rb
@@ -7,12 +7,15 @@ module AvailabilitiesHelper
   end
 
   def generate_cell(availability)
-    content_tag :td, align: 'center', class: 'checkbox_cell',
-                     onclick: "toggle(#{availability.id})",
-                     id: "cell_#{availability.id}",
-                     colspan: get_colspan(availability.time_range_id) do
-      check_box_tag 'availability_ids[]', availability.id,
-                    availability.status,
+    id = availability.id
+    time_range = availability.time_range
+
+    td_attributes = { align: 'center', class: 'checkbox_cell',
+                      onclick: "toggle(#{id})", id: "cell_#{id}",
+                      colspan: calc_colspan(time_range.start_time,
+                                            time_range.end_time) }
+    content_tag :td, td_attributes do
+      check_box_tag "availability_ids[#{id}]", id, availability.status,
                     style: 'visibility: hidden; display: none:'
     end
   end
