1518af21d6815296471a99d8baf19cc3119fc0a3
Sergio Vieri <sergio@vieri.tech>
2018-03-18 20:14:45 +0800

    Added controller spec

--- spec/controllers/availabilities_controller_spec.rb
+++ spec/controllers/availabilities_controller_spec.rb
@@ -6,10 +6,19 @@ RSpec.describe AvailabilitiesController, type: :controller do
   describe 'GET availabilities#index' do
     before do
       create_list(:time_range, 10)
-      @user = create(:user)
-      sign_in @user, scope: :user
+      sign_in create(:user), scope: :user
       get :index
     end
     it { should respond_with :ok }
+  end
+
+  describe 'POST availabilities#update_availabilities' do
+    before do
+      sign_in create(:user), scope: :user
+      create_list(:time_range, 10)
+      post :update_availabilities, params: { availability_ids: {} }
+    end
+
+    it { should redirect_to availabilities_path }
   end
 end
