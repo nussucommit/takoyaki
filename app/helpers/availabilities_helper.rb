54db12446a78ea47cb55aaeed41986074fcca702
Sergio Vieri <sergio@vieri.tech>
2018-03-20 14:21:11 +0800

    Fixed helper

--- app/helpers/availabilities_helper.rb
+++ app/helpers/availabilities_helper.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 module AvailabilitiesHelper
-  def generate_cell(day, timerrange)
-    generate(@availabilities[[day, time_range.id]], calc_colspan(time_range))
+  def generate_cell(day, time_range)
+    generate(@availabilities[[day, time_range.id]],
+             calc_colspan(time_range.start_time, time_range.end_time))
   end
 
   def generate(availability, col_span)
