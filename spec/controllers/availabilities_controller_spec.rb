3268c611c4cf5632eeb82949563c21ad3a7098dc
Sergio Vieri <sergio@vieri.tech>
2018-03-20 15:00:03 +0800

    Added update availability controller spec

--- spec/controllers/availabilities_controller_spec.rb
+++ spec/controllers/availabilities_controller_spec.rb
@@ -6,19 +6,50 @@ RSpec.describe AvailabilitiesController, type: :controller do
   describe 'GET availabilities#index' do
     before do
       create_list(:time_range, 10)
-      sign_in create(:user), scope: :user
+      @user = create(:user)
+      sign_in @user, scope: :user
       get :index
     end
     it { should respond_with :ok }
+
+    it 'creates availability' do
+      TimeRange.all.to_a.all?{ |time_range| Availability.exists?(user: @user, time_range: time_range) }
+    end
   end
 
   describe 'POST availabilities#update_availabilities' do
     before do
-      sign_in create(:user), scope: :user
+      @user = create(:user)
+      sign_in @user, scope: :user
       create_list(:time_range, 10)
-      post :update_availabilities, params: { availability_ids: {} }
+      get :index
     end
 
-    it { should redirect_to availabilities_path }
+    it 'redirects to availabilities_path' do
+      post :update_availabilities, params: { availability_ids: [] }
+      should redirect_to availabilities_path
+    end
+
+    it 'should not create new availability' do
+      expect do
+        post :update_availabilities, params: { availability_ids: [] }
+      end.to change(Availability, :count).by(0)
+    end
+
+    it 'updates availability status to true' do
+      @availability = Availability.take
+      @availability.update(status: false)
+      expect do
+        post :update_availabilities, params: { availability_ids: [@availability.id] }
+      end.to change { Availability.find(@availability.id).status }.from(false).to(true)
+    end
+
+    it 'updates availability status to false' do
+      @availability = Availability.take
+      @availability.update(status: true)
+      expect do
+        post :update_availabilities, params: { availability_ids: [] }
+      end.to change { Availability.find(@availability.id).status }.from(true).to(false)
+    end
   end
 end
