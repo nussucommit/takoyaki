084d497b2fb3ef04926c4392f33ec288bd9069f2
Sergio Vieri <sergio@vieri.tech>
2018-03-24 13:34:48 +0800

    First success test email

--- app/mailers/generic_mailer.rb
+++ app/mailers/generic_mailer.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class GenericMailer < ApplicationMailer
+  default from: %("Mailgun Sandbox" <postmaster@sandbox8f696e611a6e4906a977c007f8971322.mailgun.org>)
+
+  def test_email(user)
+    @user = user
+    puts user.email
+    mail(to: @user.email, subject: 'Test Email')
+  end
 end
