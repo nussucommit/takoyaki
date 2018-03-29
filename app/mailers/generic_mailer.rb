b77274fe44c4afda36910614469ba130a7d240af
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-03-29 23:32:26 +0800

    Remove test_email

--- app/mailers/generic_mailer.rb
+++ app/mailers/generic_mailer.rb
@@ -4,12 +4,6 @@ class GenericMailer < ApplicationMailer
   default from: %("Mailgun Sandbox" \
     <postmaster@sandbox8f696e611a6e4906a977c007f8971322.mailgun.org>)
 
-  def test_email(user)
-    @user = user
-    Rails.logger.debug user.email
-    mail(to: @user.email, subject: 'Test Email')
-  end
-
   def drop_duty(duty)
     @duty = duty
     mail(to: all_users_with_name,
