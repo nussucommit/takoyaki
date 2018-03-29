e0258decb2c58879a9e408875b9db0391f2a5883
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-03-30 01:06:03 +0800

    Update files after migration

--- spec/models/availability_spec.rb
+++ spec/models/availability_spec.rb
@@ -1,30 +1,27 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/LineLength
 # == Schema Information
 #
 # Table name: availabilities
 #
 #  id            :integer          not null, primary key
 #  user_id       :integer
-#  day           :integer          not null
 #  time_range_id :integer
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
 #  status        :boolean          not null
+#  day           :integer
 #
 # Indexes
 #
-#  index_availabilities_on_time_range_id                      (time_range_id)
-#  index_availabilities_on_user_id                            (user_id)
-#  index_availabilities_on_user_id_and_time_range_id_and_day  (user_id,time_range_id,day) UNIQUE
+#  index_availabilities_on_time_range_id  (time_range_id)
+#  index_availabilities_on_user_id        (user_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (time_range_id => time_ranges.id)
 #  fk_rails_...  (user_id => users.id)
 #
-# rubocop:enable Metrics/LineLength
 
 require 'rails_helper'
 
