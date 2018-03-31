ab5e6dba3f5d5463c8c549c36eec7b42bc9c52f2
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-03-31 23:39:35 +0800

    Fix rubocop

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe ApplicationHelper, type: :helper do
   describe '#calc_colspan' do
     it 'calculates colspan correctly' do
-      expect(calc_colspan(Time.strptime('0900','%H%M'),
+      expect(calc_colspan(Time.strptime('0900', '%H%M'),
                           Time.strptime('1100', '%H%M')))
         .to eq(4)
     end
