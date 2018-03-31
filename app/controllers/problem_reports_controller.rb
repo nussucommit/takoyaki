9fd6bf2cc656474d8062fd0a558dae13c4cae24e
Kwee Lung Sin <lungsin@Klungss-MBP.lan>
2018-03-31 17:47:28 +0800

    add change remarks and notif when create new problem

--- app/controllers/problem_reports_controller.rb
+++ app/controllers/problem_reports_controller.rb
@@ -2,11 +2,11 @@
 
 class ProblemReportsController < ApplicationController
   def index 
-    @problem_reports = ProblemReport.all
+    @problem_reports = ProblemReport.order(id: :desc)
+    @problem_reports.where!(is_fixed: false)
   end
 
   def new
-    # @test = "bum"
   end
   
   def create
@@ -23,14 +23,24 @@ class ProblemReportsController < ApplicationController
     @problem_report.is_blocked = false
     @problem_report.place_id = Place.find_by_name(params[:venue_AS8] ? 'AS8' : 'YIH').id
     @problem_report.place = Place.find_by_name(params[:venue_AS8] ? 'AS8' : 'YIH')
+    
     if @problem_report.save!
+      flash[:notice] = "Success"
       redirect_to problem_reports_path
     else 
-      redirect_to new_problem_report_path
+      flash[:notice] = "Fail"
+      render new_problem_report_path
     end
   end
   
-  def update; end
-
-  def delete; end
+  def update
+    @problem_report = ProblemReport.find(params[:id])
+    @problem_report.last_update_user_id = current_user.id
+    if params[:remarks]
+      @problem_report.remarks = params[:remarks]
+    end
+    
+    @problem_report.save!
+    redirect_to problem_reports_path
+  end
 end
