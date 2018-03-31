a35dd41994e33bf9da60596183128a986ce0eb15
Guek Ling <yeoyueling@gmail.com>
2018-03-31 22:01:29 +0800

    scrollToCurrentTime()

--- app/assets/javascripts/duties.js
+++ app/assets/javascripts/duties.js
@@ -1,3 +1,6 @@
+var startTime = 8;
+var scrollLeftPx = 150;
+
 function toggleSidebar() {
   $('#announcement-toggle-btn').on('click', function() {
     if ($('#announcement-sidebar').css('display') == "block") {
@@ -16,7 +19,20 @@ function toggleSidebar() {
   });
 }
 
+function scrollToCurrentTime() {
+  var date = new Date();
+  var coeff = 1000 * 60 * 60;
+  var rounded = new Date(Math.floor(date.getTime() / coeff) * coeff);
+  var current_hour = rounded.getHours();
+  var offset = current_hour - startTime;
+
+  if (offset > 0) {
+    $('.schedule-right').scrollLeft(scrollLeftPx * offset);
+  }
+}
+
 function load() {
+  scrollToCurrentTime();
   toggleSidebar();
 }
 
