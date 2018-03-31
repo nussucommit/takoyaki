25f3f08ae39103260171a1a53421e17e58af3cd1
Kwee Lung Sin <lungsin@Klungss-MBP.lan>
2018-04-01 05:05:53 +0800

    addd problem reports controller spec

--- spec/controllers/problem_reports_controller_spec.rb
+++ spec/controllers/problem_reports_controller_spec.rb
@@ -3,4 +3,114 @@
 require 'rails_helper'
 
 RSpec.describe ProblemReportsController, type: :controller do
+  describe "GET problem_reports#index" do
+    before do
+      sign_in create(:user)
+      get :index
+    end
+    
+    it { should respond_with :ok }
+  end
+  
+  describe "POST problem_reports#create" do
+    before do
+      sign_in create(:user)
+      @venue = create(:place).name
+    end
+    
+    it "should redirect to problem_reports_path" do
+      post :create, params: {venue: @venue, computer_number: "A10", description: "I'm too rich"} 
+      should redirect_to problem_reports_path
+    end
+    
+    it "should redirect to new_problem_report_path" do
+      post :create, params: {venue: @venue, computer_number: "", description: "I'm too rich"} 
+      should redirect_to new_problem_report_path
+      
+      post :create, params: {venue: @venue, computer_number: "", description: ""} 
+      should redirect_to new_problem_report_path
+      
+      post :create, params: {venue: @venue, computer_number: "A10", description: ""} 
+      should redirect_to new_problem_report_path
+    end
+  end
+  
+  describe "GET problem_reports#new" do
+    before do
+      sign_in create(:user)
+      get :new
+    end
+    
+    it { should respond_with :ok }
+  end
+  
+  describe "PATCH problem_reports#update" do
+    before do
+      sign_in create(:user)
+      @report = create(:problem_report)
+    end
+    
+    it "should redirect to problem_reports_path" do
+      patch :update, params: {id: @report.id, remarks: "I'm super Rich"}
+      should redirect_to problem_reports_path
+      
+      patch :update, params: {id: @report.id, is_fixable: "false"}
+      should redirect_to problem_reports_path
+      
+      patch :update, params: {id: @report.id, is_fixed: "true"}
+      should redirect_to problem_reports_path
+      
+      patch :update, params: {id: @report.id, is_blocked: "true"}
+      should redirect_to problem_reports_path
+      
+      patch :update, params: {id: @report.id, is_critical: "true"}
+      should redirect_to problem_reports_path
+    end
+    
+    it "should change the remarks" do
+      expect{
+        patch :update, params: {id: @report.id, remarks: "I'm super duper Rich"}
+      }.to change{ ProblemReport.find(@report.id).remarks }.to("I'm super duper Rich")
+    end
+    
+    it "should change the is_fixable" do
+      expect{
+        patch :update, params: {id: @report.id, is_fixable: "true"}
+      }.to change{ ProblemReport.find(@report.id).is_fixable }.to(true)
+      
+      expect{
+        patch :update, params: {id: @report.id, is_fixable: "false"}
+      }.to change{ ProblemReport.find(@report.id).is_fixable }.to(false)
+    end
+    
+    it "should change the is_fixed" do
+      expect{
+        patch :update, params: {id: @report.id, is_fixed: "false"}
+      }.to change{ ProblemReport.find(@report.id).is_fixed }.to(true)
+      
+      expect{
+        patch :update, params: {id: @report.id, is_fixed: "true"}
+      }.to change{ ProblemReport.find(@report.id).is_fixed }.to(false)
+    end
+    
+    it "should change the is_blocked" do
+      expect{
+        patch :update, params: {id: @report.id, is_blocked: "false"}
+      }.to change{ ProblemReport.find(@report.id).is_blocked }.to(true)
+      
+      expect{
+        patch :update, params: {id: @report.id, is_blocked: "true"}
+      }.to change{ ProblemReport.find(@report.id).is_blocked }.to(false)
+    end
+    
+    it "should change the is_critical" do
+      expect{
+        patch :update, params: {id: @report.id, is_critical: "false"}
+      }.to change{ ProblemReport.find(@report.id).is_critical }.to(true)
+      
+      expect{
+        patch :update, params: {id: @report.id, is_critical: "true"}
+      }.to change{ ProblemReport.find(@report.id).is_critical }.to(false)
+    end
+  end
 end
