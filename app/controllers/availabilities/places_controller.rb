6270e24751f5c8fb09dea3417c8c5aeace13cd04
Sergio Vieri <sergio.vieri.hp@gmail.com>
2018-04-03 13:21:45 +0800

    Added edit places view and controller

--- app/controllers/availabilities/places_controller.rb
+++ app/controllers/availabilities/places_controller.rb
@@ -7,14 +7,29 @@ module Availabilities
     end
 
     def edit
+      @place = Place.find(params[:id])
       @time_ranges = TimeRange.order(:start_time)
       @timeslots = Hash[Timeslot.joins(:time_range)
                                 .map do |timeslot|
-                          [[timeslot.day, timeslot.time_range_id], timeslot]
+                          [[Availability.days[timeslot.day], timeslot.time_range_id], timeslot]
                         end
       ]
+      @start_time = start_time
+      @end_time = end_time
     end
 
     def update; end
+
+    private
+
+
+    def start_time
+      @time_ranges.first.start_time.beginning_of_hour
+    end
+
+    def end_time
+      (@time_ranges.last.end_time - 1).beginning_of_hour + 1.hour
+    end
+
   end
 end
