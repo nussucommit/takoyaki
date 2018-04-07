780d3ebc854c50a299764be25241d6dd2cc13af9
Guek Ling <yeoyueling@gmail.com>
2018-04-07 21:09:19 +0800

    Fix comment issues

--- app/assets/javascripts/duties.js
+++ app/assets/javascripts/duties.js
@@ -1,10 +1,9 @@
 var ONE_HOUR_TO_MILLISECONDS = 1000 * 60 * 60;
-var START_TIME = 8;
 var SCROLL_LEFT_PX = 150;
 var MEDIUM_SCREEN_SIZE = 768;
 
 function setDutyTableButtons() {
-  if ($(window).width() <= mediumScreenSize) {
+  if ($(window).width() <= MEDIUM_SCREEN_SIZE) {
     $('.duty-table-button').removeClass('float-sm-right');
     $('.duty-table-title').removeClass('float-left');
   }
@@ -36,21 +35,35 @@ function toggleSidebar() {
   });
 }
 
-function scrollToCurrentTime() {
+function scrollToCurrentTime(startTime) {
   var date = new Date();
   var rounded = new Date(Math.floor(date.getTime() / ONE_HOUR_TO_MILLISECONDS) * ONE_HOUR_TO_MILLISECONDS);
-  var current_hour = rounded.getHours();
-  var offset = current_hour - START_TIME;
+  var currentHour = rounded.getHours();
+  var offset = currentHour - startTime;
 
   if (offset > 0) {
     $('.schedule-right').scrollLeft(SCROLL_LEFT_PX * offset);
   }
 }
 
+function colourScheduleTable(numOfPlaces) {
+  var nthChildInt = numOfPlaces * 2;
+
+  for (offset = 0; offset < numOfPlaces; offset++) {
+    $('.schedule-container tr:nth-child(' + nthChildInt + 'n -' + offset + ')').css({
+      "background-color": "#f6e1af"
+    });
+  }
+}
+
 function load() {
-  scrollToCurrentTime();
+  var START_TIME = $('#duty-start-time').data('start-time');
+  var NUM_OF_PLACES = $('#num-of-places').data('num-of-places');
+
+  scrollToCurrentTime(START_TIME);
   toggleSidebar();
   setDutyTableButtons();
+  colourScheduleTable(NUM_OF_PLACES);
 }
 
 $(document).on('turbolinks:load', load);
