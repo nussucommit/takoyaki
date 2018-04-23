791ae05e299e34bc0b56a6005379bec190e83efe
Guek Ling <yeoyueling@gmail.com>
2018-04-23 13:19:26 +0800

    Modify Problem Report Table

--- app/helpers/problem_reports_helper.rb
+++ app/helpers/problem_reports_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module ProblemReportsHelper
+  def flash_class(level)
+    case level
+    when :notice then 'alert-box alert alert-success'
+    when :alert then 'alert-box alert alert-danger'
+    end
+  end
 end
