109f20c623d0006ab49b2b4e10a9956533ea6777
Guek Ling <yeoyueling@gmail.com>
2018-04-24 13:49:18 +0800

    Checks current user before updating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,27 +15,30 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find params[:id]
-    if @user.update_with_password user_params
-      bypass_sign_in @user
-      redirect_to users_path, notice: 'Password Successfully Updated'
+
+    if current_user.id === params[:id].to_i
+      if @user.update_with_password user_params
+        bypass_sign_in @user
+        redirect_to users_path, notice: 'Password Successfully Updated'
+      else
+        redirect_to users_path, alert: 'Fail to Update Password'
+      end
     else
-      redirect_to users_path, alert: 'Fail to Update Password'
+      redirect_to users_path, alert: 'Not Authorised'
     end
   end
 
   def allocate_role
-    if can?(:manage, User)
-      @user = User.find params[:id]
-    end
+    @user = User.find params[:id] if can?(:manage, User)
   end
 
   def update_role
     @user = User.find params[:id]
-    
+
     if can?(:manage, User)
       if @user.update role_params
         Role::ROLES.each do |r|
-         role_adder(@user, r)
+          role_adder(@user, r)
         end
 
         redirect_to users_path, notice: 'Role Updated Succesfully'
@@ -62,10 +65,10 @@ class UsersController < ApplicationController
   end
 
   def role_adder(user, role)
-   if params[role] == '1'
-     user.add_role(role)
-   elsif params[role] == '0'
-     user.remove_role(role)
-   end
- end
+    if params[role] == '1'
+      user.add_role(role)
+    elsif params[role] == '0'
+      user.remove_role(role)
+    end
+  end
 end
