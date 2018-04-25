8da7f15e7ca521a621d9805f960e1da0bc3c4194
Herbert Ilhan Tanujaya <mail@herbert.id>
2018-04-25 10:58:57 +0800

    1337 h4x what is DRY?

---
+++ app/controllers/duties/places_controller.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+module Duties
+  class PlacesController < ApplicationController
+    load_and_authorize_resource
+    before_action :authenticate_user!
+
+    def index; end
+
+    def edit
+      @start_date = (params[:start_date] || Time.zone.today.beginning_of_week)
+                    .to_date
+      @end_date = @start_date.to_date + 6.days
+      @users = User.all
+      load_availabilities
+      load_timeslots
+      @disable_viewport = true
+    end
+
+    def update
+      duty_params.each { |id, user_id| Duty.find(id).update(user_id: user_id) }
+      redirect_to edit_duties_place_path(@place),
+                  notice: 'Duties successfully updated!'
+    end
+
+    private
+
+    def load_availabilities
+      @availabilities = Hash.new { |h, k| h[k] = Set[] }
+      Availability.where(status: true).each do |a|
+        @availabilities[
+          [Availability.days[a.day], a.time_range_id]] << a.user_id
+      end
+    end
+
+    def load_timeslots
+      @timeslots = Hash.new { |h, k| h[k] = [] }
+      Timeslot.where(place_id: params[:id]).includes(:time_range)
+              .each do |timeslot|
+        @timeslots[Availability.days[timeslot.day]] << timeslot
+      end
+    end
+
+    def update_timeslot(timeslot)
+      selected_user_id = params["#{Availability.days[timeslot.day]}" \
+                                "#{timeslot.time_range_id}"]
+      return if timeslot.default_user_id == selected_user_id
+      timeslot.update(default_user_id: selected_user_id)
+    end
+
+    def duty_params
+      params.require(:duty)
+    end
+  end
+end
