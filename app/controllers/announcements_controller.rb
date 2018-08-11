dd87de3348412b8950356354d8a8b7cf76a6de77
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-08-11 14:11:10 +0800

    Allow MC to post announcements

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,28 +1,38 @@
 # frozen_string_literal: true
 
 class AnnouncementsController < ApplicationController
-  load_and_authorize_resource
-
   def index
     @announcements = Announcement.order(created_at: :desc)
   end
 
   def create
-    announcement = Announcement.new announcement_params
+    if current_user.mc
+      announcement = Announcement.new announcement_params
 
-    redirect_to duties_path if announcement.save
+      redirect_to duties_path if announcement.save
+    else
+      redirect_to duties_path
+    end
   end
 
   def update
-    announcement = Announcement.find params[:id]
+    if current_user.mc
+      announcement = Announcement.find params[:id]
 
-    redirect_to duties_path if announcement.update announcement_params
+      redirect_to duties_path if announcement.update announcement_params
+    else
+      redirect_to duties_path
+    end
   end
 
   def destroy
-    announcement = Announcement.find params[:id]
+    if current_user.mc
+      announcement = Announcement.find params[:id]
 
-    redirect_to duties_path if announcement.destroy
+      redirect_to duties_path if announcement.destroy
+    else
+      redirect_to duties_path
+    end
   end
 
   private
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -13,8 +13,6 @@ class Ability
     can :manage, User, id: user.id
     cannot %i[create destroy allocate_roles update_roles show_full], User
 
-    can :read, Announcement
-
     user.roles.each { |role| __send__(role.name.downcase) }
   end
 
