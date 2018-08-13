4ad7a5bcad6014babf7713b07c510cebf790dabc
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-08-13 16:45:42 +0800

    Relax User association to optional in ProblemReport model to allow user deletion

--- app/models/problem_report.rb
+++ app/models/problem_report.rb
@@ -27,9 +27,9 @@
 
 class ProblemReport < ApplicationRecord
   validates :computer_number, :place_id, :description, presence: true
-  belongs_to :reporter_user, class_name: 'User',
+  belongs_to :reporter_user, class_name: 'User', optional: true,
                              inverse_of: :reported_problem_reports
-  belongs_to :last_update_user, class_name: 'User',
+  belongs_to :last_update_user, class_name: 'User', optional: true,
                                 inverse_of: :last_updated_problem_reports
   belongs_to :place
 end
