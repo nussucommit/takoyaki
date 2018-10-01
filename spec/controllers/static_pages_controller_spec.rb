ede01b70d526ada9007bf45cbbcd62d07b615c54
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-10-01 16:03:20 +0800

    Add guide and tests (#152)

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -3,4 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
+  describe 'GET static_pages#guide' do
+    before do
+      sign_in create(:user)
+      get :guide
+    end
+
+    it { should respond_with :ok }
+  end
 end
