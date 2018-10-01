4b67d4f9fcb3f8d457d374f41bf2dcb1af4f53bb
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-10-01 17:24:13 +0800

    Make guide redirect instead (#153)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class StaticPagesController < ApplicationController
-  def guide; end
+  GUIDE_URL = 'https://docs.google.com/document/d/1rNjei4GOOAiDBk4p9EP4ipvmTcq2jJxaY-bIR8XUCVw/edit?usp=sharing'
+
+  def guide
+    redirect_to GUIDE_URL
+  end
 
   def grab_duty; end
 end
