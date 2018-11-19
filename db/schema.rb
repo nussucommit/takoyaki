dfea3b9e7bbeae19a7df7afdfc6dae6a51a3f005
Guek Ling <yeoyueling@gmail.com>
2018-11-19 22:41:51 +0800

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,21 @@ ActiveRecord::Schema.define(version: 2018_11_14_023532) do
     t.index ["user_id"], name: "index_availabilities_on_user_id"
   end
 
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string "locked_by"
+    t.string "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
+  end
+
   create_table "duties", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "timeslot_id"
