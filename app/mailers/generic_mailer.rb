f0a80bd7cc025cc7a91789234724ef0a5eaceb3d
Guek Ling <yeoyueling@gmail.com>
2018-11-19 22:42:46 +0800

    Change mail to batch sending

--- app/mailers/generic_mailer.rb
+++ app/mailers/generic_mailer.rb
@@ -1,24 +1,41 @@
 # frozen_string_literal: true
 
 class GenericMailer < ApplicationMailer
-  default from: %("NUSSU commIT" <duty@#{ENV['MAILGUN_DOMAIN']}>)
-
   def drop_duties(duties, user_ids)
     @from = duties.first.user.username
     @date = duties.first.date
-    # Prevent (n + 1) queries
-    @times = process_duties_times(
-      Duty.includes(:time_range).find(duties.map(&:id))
-    )
+    @times = process_duties_times(duties)
     @venue = duties.first.place.name
-    mail(to: users_with_name(user_ids),
-         subject: generate_drop_duty_subject_detailed(@times, @date, @venue))
+
+    mg_client = Mailgun::Client.new(ENV['MAILGUN_API_KEY'])
+    mb_obj = Mailgun::BatchMessage.new(mg_client, ENV['MAILGUN_DOMAIN'])
+    mb_obj.from("duty@#{ENV['MAILGUN_DOMAIN']}", {"first" => "NUSSU commIT"});
+    mb_obj.subject(generate_drop_duty_subject_detailed(@times, @date, @venue));
+    html_output = render_to_string template: "generic_mailer/drop_duties"
+    mb_obj.body_html(html_output);
+
+    users_with_name(user_ids).each do |u|
+      mb_obj.add_recipient(:to, u[1], {"first" => u[0]});
+    end
+
+    mb_obj.finalize
   end
 
   def problem_report(problem)
     @problem = problem
-    mail(to: users_with_name(User.where(cell: 'technical').pluck(:id)),
-         subject: 'New computer problem')
+
+    mg_client = Mailgun::Client.new(ENV['MAILGUN_API_KEY'])
+    mb_obj = Mailgun::BatchMessage.new(mg_client, ENV['MAILGUN_DOMAIN'])
+    mb_obj.from("duty@#{ENV['MAILGUN_DOMAIN']}", {"first" => "NUSSU commIT"});
+    mb_obj.subject("New Computer Problem");
+    html_output = render_to_string template: "generic_mailer/problem_report"
+    mb_obj.body_html(html_output);
+
+    users_with_name(User.where(cell: 'technical').pluck(:id)).each do |u|
+      mb_obj.add_recipient(:to, u[1], {"first" => u[0]});
+    end
+
+    mb_obj.finalize
   end
 
   private
@@ -31,9 +48,7 @@ class GenericMailer < ApplicationMailer
   end
 
   def users_with_name(user_ids)
-    User.find(user_ids)&.pluck(:username, :email)&.map do |u|
-      %("#{u[0]}" <#{u[1]}>)
-    end
+    User.find(user_ids)&.pluck(:username, :email)
   end
 
   def process_duties_times(duties)
