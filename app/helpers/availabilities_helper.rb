9af6269665d57ae96d87b35bf5c6ae90a9b07309
Christian James Welly <chrisjwelly@gmail.com>
2019-02-14 15:36:48 +0800

    Modify function to be used for one user

--- app/helpers/availabilities_helper.rb
+++ app/helpers/availabilities_helper.rb
@@ -15,9 +15,9 @@ module AvailabilitiesHelper
 
   def stylise_user(user)
     if user[:mc]
-      content_tag(:span, user[:username], class: 'mc')
+      content_tag(:span, user[:username] + "(10)", class: 'mc') 
     else
-      user[:username]
+      user[:username] + "(10)" #call function to return number of hours
     end
   end
 
@@ -75,9 +75,9 @@ module AvailabilitiesHelper
     end
   end
 
-  def total_user_hours
-    relevant_duties = Availability.includes(:time_range)
-                                  .where(user_id: current_user.id,
+  def total_user_hours(user)
+    relevant_duties = user.includes(:time_range)
+                          .where(user_id: user.id,
                                  date: @start_date..@end_date,
                                  free: false, request_user_id: nil)
     total_seconds = relevant_duties.map do |d|
