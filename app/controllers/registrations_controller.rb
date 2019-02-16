4dc1a68c93c9e97c2438832bb18a7359b5b56b56
Loop97 <e0319186@u.nus.edu>
2019-02-16 10:09:15 +0800

    Fixed with guard clause style

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -16,10 +16,10 @@ class RegistrationsController < Devise::RegistrationsController
     resource.save
     success = resource.save
     yield resource if block_given?
-    if success
-      redirect_to users_path,
-                  alert: resource.errors.full_messages.join(',')
-    end
+    return unless success
+
+    redirect_to users_path,
+                alert: resource.errors.full_messages.join(',')
   end
 
   def after_sign_up_path_for(_resource)
