29cedbbef00eb088b846fcf6f498b5e878c64b02
Loop97 <e0319186@u.nus.edu>
2019-02-16 11:10:21 +0800

    Added test case for email and username uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -54,6 +54,8 @@ RSpec.describe User, type: :model do
   it { should have_many(:availabilities).dependent(:destroy) }
   it { should validate_presence_of(:cell) }
   it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_uniqueness_of(:username) }
   it { should define_enum_for(:cell).with_values(User::CELLS) }
   it 'has false as default value for mc' do
     expect(create(:user).mc).to eq(false)
