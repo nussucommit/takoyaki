2d7724cc4a8fcf70108f8b4a4b78505e7fc2c8ac
Yehezkiel Raymundo Theodoroes <yehezkieltheodoroes@yahoo.co.id>
2019-03-16 09:52:55 +0800

    Remove start_date

--- app/controllers/duties_controller.rb
+++ app/controllers/duties_controller.rb
@@ -10,11 +10,10 @@ class DutiesController < ApplicationController
   end
 
   def generate_duties
-    start_date = (params[:start_date] || Time.zone.today.beginning_of_week)
-                 .to_date
-    end_date = start_date + (params[:num_weeks].to_i * 7 - 1).days
-    Duty.generate(start_date, end_date)
-    redirect_to duties_path(start_date: start_date),
+    set_start_end_dates
+    end_date = @start_date + (params[:num_weeks].to_i * 7 - 1).days
+    Duty.generate(@start_date, end_date)
+    redirect_to duties_path(start_date: @start_date),
                 notice: 'Duties successfully generated!'
   end
 
