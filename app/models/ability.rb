81f6742f058349ac6fffe5259eb1d5159cdfc906
Hilda-Ang <E0196709@u.nus.edu>
2019-04-13 09:48:07 +0800

    Fix changing password as admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -12,6 +12,7 @@ class Ability
 
     can :manage, User, id: user.id
     cannot %i[create destroy allocate_roles update_roles show_full], User
+    cannot :change_without_password, User
 
     cannot :manage, Setting
 
