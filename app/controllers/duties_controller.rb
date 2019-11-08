eb789770f365568cd5ad9772d31bcf4ed166465a
NicholasCF <nicholas.cristian@yahoo.com>
2019-11-08 20:24:31 +0800

    Move parts of controller code to excel-generating code

--- app/controllers/duties_controller.rb
+++ app/controllers/duties_controller.rb
@@ -63,18 +63,8 @@ class DutiesController < ApplicationController
   end
 
   def export
-    @start_date = (Time.zone.today - 1.month).beginning_of_month.to_date
-    @end_date = (Time.zone.today.beginning_of_month - 1.day).to_date
-
-    @places = Place.all.sort_by do |p|
-      p.name
-    end
-
     @header_iter = generate_header_iter
-
-    @relevant_duties = Duty.includes(%i[time_range place user])
-                           .where(date: @start_date..@end_date)
-
+    
     respond_to do |format|
       format.xlsx {
         response.headers['Content-Disposition'] = "attachment; filename=duties.xlsx"
